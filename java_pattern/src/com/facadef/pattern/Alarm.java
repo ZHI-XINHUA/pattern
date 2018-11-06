67bc4e3e060b581b5b5825667942c6785b87f31e
ZHI-XINHUA <1960881192@qq.com>
2018-11-06 11:00:35 +0800

    门面模式

--- java_pattern/src/com/facadef/pattern/Alarm.java
+++ java_pattern/src/com/facadef/pattern/Alarm.java
@@ -1,5 +1,8 @@
 package com.facadef.pattern;
 
+/**
+ * 子系统（Subsystem）角色：可以同时有一个或多个子系统。每个子系统都可以被客户端直接调用，或者被门面角色调用。子系统并不知道门面的存在，对于子系统而已，门面仅仅是另外一个客户端而已
+ */
 public class Alarm {
 
     public void activate(){
--- java_pattern/src/com/facadef/pattern/SecurityFacade.java
+++ java_pattern/src/com/facadef/pattern/SecurityFacade.java
@@ -1,5 +1,9 @@
 package com.facadef.pattern;
 
+/**
+ * 门面（Facade）角色：客户端可以调用这个角色的方法。此角色知晓相关的（一个或多个）子系统的功能和责任。在正常情况下，本角色会将所有从客户端发来的请求
+ * 委派到相应的子系统去
+ */
 public class SecurityFacade {
     private static  Camera camera;
     private static  Light light;
